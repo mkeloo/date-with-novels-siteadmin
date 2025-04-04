269dc770d3019dbad376a49f2509a339aa1fc9c7
mkeloo <mkeloo@ufl.edu>
2025-04-04 03:14:08 -0400

    Refactor PromosPage to use Card component for improved layout and styling

--- app/dashboard/promotion-&-testimonials/promos/page.tsx
+++ app/dashboard/promotion-&-testimonials/promos/page.tsx
@@ -1,7 +1,10 @@
+import { Card } from '@/components/ui/card'
 import React from 'react'
 
 export default function PromosPage() {
     return (
-        <div>PromosPage</div>
+        <Card className="w-full h-full flex items-center justify-center gap-4 p-4">
+            Promos Page
+        </Card>
     )
 }
